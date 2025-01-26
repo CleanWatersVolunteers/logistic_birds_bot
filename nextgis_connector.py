642ab2c26a8a7f1c6357b5e704e246575c198e4a
strelok92 <sergeykokin92@gmail.com>
2025-01-26 22:15:48 +0300

    issues #1 #2 #3 was resolved

--- nextgis_connector.py
+++ nextgis_connector.py
@@ -1,13 +1,11 @@
 from datetime import datetime
 import pytz
 import requests
+import re
 import json
 
-# time_tz = lambda tz: datetime.now(pytz.timezone(tz))
-# curr_time = lambda : time_tz('Etc/GMT+6')
-
 time_tz = lambda tz: datetime.utcnow().astimezone(pytz.timezone(tz))
-curr_time = lambda : time_tz('Etc/GMT-3').isoformat().split('+')[0]
+curr_time = lambda : time_tz('Etc/GMT-6').isoformat().split('+')[0]
 
 ngw_host = 'https://blacksea-monitoring.nextgis.com'
 f_lgn = open('nextgis_login', 'r')
@@ -18,66 +16,157 @@ auth = (f_lgn.read(),f_pass.read())
 f_lgn.close()
 f_pass.close()
 
-def create_request_fields(lat,lon,car=None,status=None,cargo_type=None,end_route=None)->dict:
-    feature = dict()
-    feature['fields'] = dict()
-    feature['fields']['lat'] = lat
-    feature['fields']['long'] = lon
-    if car != None:
-        feature['fields']['car'] = car
-    if cargo_type != None:
-        feature['fields']['cargo_type'] = cargo_type
-    if status != None:
-        feature['fields']['status'] = status
-    if end_route != None:
-        feature['fields']['end_route'] = end_route
-    feature['fields']['dt_auto'] = curr_time()
-    return feature
+class NextGIS:
+    ngw_host = 'https://blacksea-monitoring.nextgis.com'
+    url_feature = ngw_host + '/api/resource/' + str(195) +'/feature/'
 
-def add_point_track(tg_link)->int:
-    print("[..] Add point to",tg_link)
+    @classmethod
+    def _get(cls, url):
+        print("[..] GET:",url)
+        response = requests.get(url, auth=cls.__auth)
+        if response.status_code != 200:
+            print("[!!] GET code", response.status_code, response.text)
+            return response.status_code, {}
+        return response.status_code, response.json()
+    @classmethod
+    def _post(cls, url, data):
+        print("[..] POST:",url)
+        if data:
+            response = requests.post(url, auth=cls.__auth,data=json.dumps(data))
+        else:
+            response = requests.post(url, auth=cls.__auth, data=None)
+        if response.status_code != 200:
+            print("[!!] POST code", response.status_code, response.text)
+            return response.status_code, {}
+        return response.status_code, response.json()
+    @classmethod
+    def _put(cls, url, data):
+        print("[..] PUT:",url)
+        if data:
+            response = requests.put(url, auth=cls.__auth,data=json.dumps(data))
+        else:
+            response = requests.put(url, auth=cls.__auth, data=None)
+        if response.status_code != 200:
+            print("[!!] PUT code", response.status_code, response.text)
+            return response.status_code, {}
+        return response.status_code, response.json()
+    @classmethod
+    def _get_flt(cls, features)->{}:
+        req = "?"
+        for key in features:
+           req += f'{key}={features[key]}&'
+        req = req[:-1]
+        code, resp = cls._get(cls.url_feature+req)
+        if code == 200:
+            return resp
+        return {}
 
-    feature = dict()
-    feature['extensions'] = dict()
-    feature['extensions']['attachment'] = None
-    feature['extensions']['description'] = None
-    feature['fields'] = dict()
-    feature['fields']['contact_info'] = f"https://t.me/{tg_link}"
+    @classmethod
+    def init(cls)->bool:
+        cls.__db = dict()
+        try:
+            # Auth 
 
-    post_url = ngw_host + '/api/resource/' + str(195) +'/feature/?srs=4326&dt_format=iso'
-    print(feature, post_url)
-    response = requests.post(post_url, data=json.dumps(feature), auth=auth)
-    
-    if response.status_code == 200:
-        print("Feature created successfully:", response.json())
-        return response.json()['id']
-    else:
-        print("Error creating feature:", response.status_code, response.text)
+            f_lgn = open('nextgis_login', 'r')
+            f_pass = open('nextgis_pass', 'r')
+            cls.__auth = (f_lgn.read(),f_pass.read())
+            f_lgn.close()
+            f_pass.close()
+
+            # create local base
+
+            code, resp = cls._get(cls.url_feature)
+            if code != 200:
+                return False
+            for item in resp:
+                contact_link = item['fields']['contact_info']
+                if contact_link:
+                    link = re.search("https://t.me/",contact_link)
+                    if link:
+                        cls.__db[contact_link[link.span()[1]:]] = item['id']
+        except Exception as e:
+            print("[!!] GextGIS Init exeption!", e)
+            return False
+        return True
+
+    @classmethod
+    def get_id(cls,name)->int:
+        if name in cls.__db:
+            return cls.__db[name]
         return 0
+    @classmethod
+    def get_name(cls,id)->str:
+        for key in cls.__db:
+            if cls.__db[key] == id:
+                return key
+        return None
+    @classmethod
+    def get_free_list(cls,who)->{}:
+        features = cls._get_flt({"fld_end_route":"выполняется", "fld_status":who})
+        if features:
+            return features
+        return {}
 
-def update_details(point_id, car, cargo_type, status, comment):
-    print("[..] Update details", point_id, car, status,comment)
+    @classmethod
+    def new_user(cls,name)->{}:
+        if not name:
+            return {}
+        print("[..] Add new user", name)
+        feature = dict()
+        feature['extensions'] = dict()
+        feature['extensions']['attachment'] = None
+        feature['extensions']['description'] = None
+        feature['fields'] = dict()
+        feature['fields']['contact_info'] = f"https://t.me/{name}"
+        code, data = cls._post(cls.url_feature+"?srs=4326&dt_format=iso", feature)
+        if code == 200:
+            if 'id' in data:
+                cls.__db[name] = data['id']
+                return cls.get_user(name)
+        return {}
 
-    feature = create_request_fields(lat=0,lon=0,car=car,cargo_type=cargo_type, status=status)
-    feature['fields']['dt_data'] = curr_time()
+    @classmethod
+    def get_user(cls,name)->{}:
+        if not name in cls.__db:
+            return {}
+        id = cls.__db[name]
+        # code, res = cls._get(cls.url_feature+f"{id}?srs=4326&dt_format=iso")
+        code, res = cls._get(cls.url_feature+f"{id}?srs=4326")
+        if code != 200:
+            return {}
+        return res['fields']
 
-    put_url = f"{ngw_host}/api/resource/{195}/feature/{point_id}?srs=4326&dt_format=iso"
-    print(feature, put_url)
-    response = requests.put(put_url, data=json.dumps(feature), auth=auth)
-    if response.status_code != 200:
-        print("Error creating feature:", response.status_code, response.text)
+    # GEO: long,lat
+    # STATUS: car,cargo_type,status,end_route  
+    @classmethod
+    def upd_user(cls,name, details)->bool:
+        id = cls.get_id(name)
+        if id == 0:
+            return False
 
+        # main data
+        feature = dict()
+        feature['fields'] = dict()
+        for key in details:
+            feature['fields'][key] = details[key]
+        
+        # time 
+        time = curr_time()
+        feature['fields']['dt_auto'] = time
+        if "long" in details and "lat" in details:
+            feature['fields']['dt_coord'] = time 
+            feature['geom'] = 'POINT (%s %s)' % (details["long"], details["lat"])
+        if "status" in details or "end_route" in details:
+            feature['fields']['dt_data'] = time 
 
-def move_point_track(point_id, lat, lon, end_route):
-    print("[..] Modify point", point_id,lat,lon, end_route)
+        # Request
+        put_url = f"{cls.url_feature}{id}?srs=4326&dt_format=iso"
+        code,_ = cls._put(put_url, feature)
+        if code == 200:
+            return True
+        return False
 
-    feature = create_request_fields(lat=lat,lon=lon,end_route=end_route)
-    feature['fields']['dt_coord'] = curr_time()
-    feature['geom'] = 'POINT (%s %s)' % (lon, lat)
+    @classmethod
+    def complete_user(cls,name):
+        cls.upd_user(name, {"long":0, "lat":0,"end_route":"завершено"})
 
-    put_url = f"{ngw_host}/api/resource/{195}/feature/{point_id}?srs=4326&dt_format=iso"
-    print(feature, put_url)
-    response = requests.put(put_url, data=json.dumps(feature), auth=auth)
-    # print(response)
-    if response.status_code != 200:
-        print("Error creating feature:", response.status_code, response.text)
